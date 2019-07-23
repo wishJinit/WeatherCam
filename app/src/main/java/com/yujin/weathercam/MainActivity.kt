63d4c8240a10696c42422e239e9d5fcbf5c3f423
wishJinit <wishjinit@gmail.com>
2019-07-23 20:35:45 +0900

    [Feat] : TextureView 설정
     - SurfaceTexture을 사용할 수 있을 때 카메라 실행

--- app/src/main/java/com/yujin/weathercam/MainActivity.kt
+++ app/src/main/java/com/yujin/weathercam/MainActivity.kt
@@ -1,73 +1,43 @@
 package com.yujin.weathercam
 
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
+import android.graphics.SurfaceTexture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.view.TextureView
 
 class MainActivity : AppCompatActivity() {
-
     init {
         System.loadLibrary("NativeImageProcessor")
     }
 
-    /** Helper to ask camera permission.  */
-    object CameraPermissionHelper {
-        private const val CAMERA_PERMISSION_CODE = 0
-        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
-
-        /** Check to see we have the necessary permissions for this app.  */
-        fun hasCameraPermission(activity: Activity): Boolean {
-            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
-        }
-
-        /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
-        fun requestCameraPermission(activity: Activity) {
-            ActivityCompat.requestPermissions(
-                activity, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_CODE)
-        }
-
-        /** Check to see if we need to show the rationale for this permission.  */
-        fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
-            return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)
-        }
-
-        /** Launch Application Setting to grant permission.  */
-        fun launchPermissionSettings(activity: Activity) {
-            val intent = Intent()
-            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-            intent.data = Uri.fromParts("package", activity.packageName, null)
-            activity.startActivity(intent)
-        }
+    companion object{
+        private lateinit var mTextureView:TextureView
+        private lateinit var mSurfaceTextureListener:TextureView.SurfaceTextureListener
     }
-    //https://medium.com/@tylerwalker/integrating-camera2-api-on-android-feat-kotlin-4a4e65dc593f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this)
-            return
-        }
-    }
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                .show()
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this)
+
+        mTextureView = findViewById(R.id.textureView)
+        mSurfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture?, p1: Int, p2: Int) {}
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) = Unit
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean = true
+
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture?, p1: Int, p2: Int) {
+                openCamera();
             }
-            finish()
+
         }
 
-        recreate()
+        mTextureView.surfaceTextureListener = mSurfaceTextureListener
+    }
+
+    private fun openCamera() {
+
     }
 }
