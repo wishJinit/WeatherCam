021a0247bd93b0d0dfef97a7bc7ae0d2f7206bc5
wishJinit <wishjinit@gmail.com>
2019-08-15 00:21:04 +0900

    [Style] : 파일 구조 변경

--- app/src/main/java/com/yujin/weathercam/VO/WeatherVO.kt
+++ app/src/main/java/com/yujin/weathercam/VO/WeatherVO.kt
@@ -1,7 +1,8 @@
-package com.yujin.weathercam.Data
+package com.yujin.weathercam.VO
 
 
 class WeatherVO{
     var weather:String? = null
+    var weather_kr:String? = null
     var description:String? = null
 }
