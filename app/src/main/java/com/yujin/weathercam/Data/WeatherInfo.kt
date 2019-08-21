9812b16902ead4a7d71c29fa4949c9eb68450ddf
wishJinit <wishjinit@gmail.com>
2019-08-21 23:55:49 +0900

    [Feat] : 필터 RGB 컬러

--- app/src/main/java/com/yujin/weathercam/Data/WeatherInfo.kt
+++ app/src/main/java/com/yujin/weathercam/Data/WeatherInfo.kt
@@ -1,19 +1,18 @@
 package com.yujin.weathercam.Data
 
-enum class WeatherInfo (val weather_kr: String, val weather_res: Int) {
-    Thunderstorm("천둥번개", -1),
-    Drizzle("이슬비", -1),
-    Rain("비", -1),
-    Snow("눈", -1),
-    Mist("옅은 안개",-1),
-    Haze("실안개", -1),
-    Dust("황사", -1),
-    Fog("안개",-1),
-    Sand("모래", -1),
-    Ash("잿가루", -1),
-    Squall("강한 바람", -1),
-    Tornado("토네이도", -1),
-    Clear("맑음", -1),
-    Clouds("흐림", -1),
-
+enum class WeatherInfo (val weather_kr: String, val weather_res: Int, val RGBA:String) {
+    Thunderstorm("천둥번개", -1, "#00000000"),
+    Drizzle("이슬비", -1, "#00000000"),
+    Rain("비", -1, "#00000000"),
+    Snow("눈", -1, "#00000000"),
+    Mist("옅은 안개",-1, "#00000000"),
+    Haze("실안개", -1, "#00000000"),
+    Dust("황사", -1, "#00000000"),
+    Fog("안개",-1, "#00000000"),
+    Sand("모래", -1, "#00000000"),
+    Ash("잿가루", -1, "#00000000"),
+    Squall("강한 바람", -1, "#00000000"),
+    Tornado("토네이도", -1, "#00000000"),
+    Clear("맑음", -1, "#00000000"),
+    Clouds("흐림", -1, "#00000000")
 }
--- app/src/main/java/com/yujin/weathercam/VO/WeatherVO.kt
+++ app/src/main/java/com/yujin/weathercam/VO/WeatherVO.kt
@@ -5,4 +5,5 @@ class WeatherVO{
     var weather:String? = null
     var weather_kr:String? = null
     var description:String? = null
+    var filterColor:String? = null
 }
