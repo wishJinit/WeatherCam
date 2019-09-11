7a257ec38fa06f67070e076e76f87e553e749dda
wishJinit <wishjinit@gmail.com>
2019-09-11 23:17:29 +0900

    [Feat] : 날씨데이터 디폴트값 지정
    - 디폴트값 지정 (WeatherCam)

--- app/src/main/java/com/yujin/weathercam/WeatherVO.kt
+++ app/src/main/java/com/yujin/weathercam/WeatherVO.kt
@@ -5,7 +5,7 @@ import androidx.databinding.ObservableField
 
 class WeatherVO {
     var weather = ObservableField<String>()
-    var weather_kr = ObservableField<String>()
+    var weather_kr = ObservableField<String>("WeatherCam")
     var description = ObservableField<String>()
     var filterColor = ObservableField<String>("#00000000")
 }
