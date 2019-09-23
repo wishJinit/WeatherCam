5bfdb369e65add12a7da47a37403b48de8e6aeac
wishJinit <wishjinit@gmail.com>
2019-09-23 22:45:21 +0900

    [Feat] : ProgressBar 제어코드 추가

--- app/src/main/java/com/yujin/weathercam/GalleryActivity.kt
+++ app/src/main/java/com/yujin/weathercam/GalleryActivity.kt
@@ -2,11 +2,32 @@ package com.yujin.weathercam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_gallery.*
 
 class GalleryActivity : AppCompatActivity() {
+    val TAG = "GalleryActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showProgress()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        hideProgress()
+    }
+
+    fun showProgress() {
+        progressbar.visibility = View.VISIBLE
+    }
+
+    fun hideProgress() {
+        progressbar.visibility = View.INVISIBLE
     }
 }
