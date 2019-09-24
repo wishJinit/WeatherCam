e67e3ccc1252983645f47009233462635a06dee8
wishJinit <wishjinit@gmail.com>
2019-09-25 00:35:17 +0900

    [Feat] : GalleryView BackBtn Click Event 추가

--- app/src/main/java/com/yujin/weathercam/GalleryActivity.kt
+++ app/src/main/java/com/yujin/weathercam/GalleryActivity.kt
@@ -20,6 +20,10 @@ class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+        back_btn.setOnClickListener {
+            finish()
+        }
     }
 
     override fun onStart() {
