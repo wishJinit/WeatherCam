9667a39d9ae16affba89baa9d4049b6c765e9866
wishJinit <wishjinit@gmail.com>
2019-10-05 22:09:46 +0900

    [Feat] : Event Listener 추가
    - onClickListener
    - 이미지 삭제

--- app/src/main/java/com/yujin/weathercam/ImageActivity.kt
+++ app/src/main/java/com/yujin/weathercam/ImageActivity.kt
@@ -1,8 +1,12 @@
 package com.yujin.weathercam
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_image.*
 import java.io.File
 import java.io.FileInputStream
@@ -23,6 +27,35 @@ class ImageActivity : AppCompatActivity() {
             }
         }
 
+        setEventListener()
+    }
+
+    fun setEventListener(){
+        back_btn.setOnClickListener {
+            closeActivity(false)
+        }
+        delete_btn.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+                .setTitle(getString(R.string.app_name))
+                .setMessage("정말 삭제하시겠습니까?")
+                .setIcon(R.drawable.weathercam_logo)
+                .setPositiveButton("삭제") { dialogInterface: DialogInterface, i: Int ->
+                    imageFile?.let {
+                        if (it.exists() && it.delete()){
+                            showToast("이미지를 정상적으로 삭제하였습니다.")
+                            closeActivity(true)
+                        }else{
+                            showToast("이미지 삭제에 실패하였습니다.")
+                        }
+                    }
+                }
+                .setNegativeButton("취소"){ dialogInterface: DialogInterface, i: Int ->
+                    showToast("취소하였습니다.")
+                }
+            dialog.create().show()
+        }
+    }
+
     fun setImageView(file: File){
         val inputStream = FileInputStream(file)
         val options = BitmapFactory.Options()
@@ -30,5 +63,15 @@ class ImageActivity : AppCompatActivity() {
         val bitmap = BitmapFactory.decodeStream(inputStream, null, options)
         imageView.setImageBitmap(bitmap)
     }
+
+    fun showToast(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun closeActivity(isDeletion:Boolean){
+        val intent = Intent()
+        intent.putExtra("isDeletion", isDeletion)
+        setResult(0, intent)
+        finish()
     }
 }
